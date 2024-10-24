506330eb3a9c357f1df75bb7788d27a964ad4fbc
Tanmay <tanmay.json@gmail.com>
2024-10-24 18:14:32 +0530

    login context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CourseDetails from './components/fa/CourseDetails';
 import Registration from './pages/fa/Registration';
+import Dashboard from './pages/student/dashboard';
 
 const Loader = lazy(() => import('./components/Loader'));
 const Login = lazy(() => import('./pages/Login'));
@@ -17,6 +18,7 @@ function App() {
           <Route path="/fa/dashboard" element={<FaDashboard />} />
           <Route path="/fa/registration" element={<Registration />} />
           <Route path="/fa/course/:id" element={<CourseDetails />} />
+          <Route path="/student/dashboard" element={<Dashboard/>} />
 
           {/* faculty routes */}
           {/* <Route path="/faculty/dashboard" element={<FacultyDashboard />} /> */}
---
+++ client/src/pages/student/dashboard.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/Header"
+
+function Dashboard() {
+  return (
+      <div>
+          <Header/>
+      hi
+    </div>
+  )
+}
+
+export default Dashboard
