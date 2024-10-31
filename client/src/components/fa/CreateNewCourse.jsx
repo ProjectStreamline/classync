e197b3ed355ce152fadf38f1d2cd91161db5c826
Pradnya Kumbhar <kumbharpradnyaa10@gmail.com>
2024-10-31 20:41:09 +0530

    fa can create new course

--- client/src/components/fa/CreateNewCourse.jsx
+++ client/src/components/fa/CreateNewCourse.jsx
@@ -1,59 +1,53 @@
 import { useState, useContext, useEffect } from 'react';
 import { ImCross } from 'react-icons/im';
-import SelectDropdown from './SelectDropdown';
 import { CourseContext } from '../../context/CourseContext';
 import supabase from '../../config/supabaseClient';
+import Select from 'react-select';
 const CreateNewCourse = () => {
   const { faculties, fetchFaculties } = useContext(CourseContext);
   useEffect(() => {
     fetchFaculties();
   }, []);
-  // console.log(faculties);
+
   const facultyData = faculties.map((faculty) => ({
     label: faculty.faculty_name,
-    value: faculty.faculty_name,
+    value: faculty.faculty_id,
   }));
-  // console.log(facultyData);
 
-  // const semesters = [
-  //   { label: '1', value: '1' },
-  //   { label: '2', value: '2' },
-  //   { label: '3', value: '3' },
-  //   { label: '4', value: '4' },
-  //   { label: '5', value: '5' },
-  //   { label: '6', value: '6' },
-  //   { label: '7', value: '7' },
-  //   { label: '8', value: '8' },
-  // ];
-  const courseCodes = [
-    { label: 'CSE 101', value: 'CSE 101' },
-    { label: 'CSE 102', value: 'CSE 102' },
-    { label: 'CSE 103', value: 'CSE 103' },
-    { label: 'CSE 104', value: 'CSE 104' },
-    { label: 'CSE 105', value: 'CSE 105' },
-    { label: 'CSE 106', value: 'CSE 106' },
-  ];
-  // const faculties = [
-  //   { label: 'Dr. Nishant Namdev', value: 'Dr. Nishant Namdev' },
-  //   { label: 'Dr. Nishant Namdev', value: 'Dr. Nishant Namdev' },
-  //   { label: 'Dr. Nishant Namdev', value: 'Dr. Nishant Namdev' },
-  // ];
-  const lab = [
+  const labOptions = [
     { label: 'Yes', value: 'Yes' },
     { label: 'No', value: 'No' },
   ];
-  const slots = [
-    { label: 'A', value: 'A' },
-    { label: 'B', value: 'B' },
-    { label: 'C', value: 'C' },
-    { label: 'D', value: 'D' },
-    { label: 'E', value: 'E' },
-    { label: 'F', value: 'F' },
-    { label: 'G', value: 'G' },
-    { label: 'H', value: 'H' },
-  ];
+
   const [openCourse, setOpenCourse] = useState(false);
   const [courseName, setCourseName] = useState('');
+  const [courseCode, setCourseCode] = useState('');
+  const [faculty, setFaculty] = useState('');
+  const [lab, setLab] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!courseName || !courseCode || !faculty || !lab) {
+      console.log('Please fill all the fields');
+      return;
+    }
+
+    const { data, error } = await supabase.from('courses').insert([
+      {
+        course_code: courseCode,
+        course_name: courseName,
+        lab: lab.value,
+        faculty: faculty.value,
+      },
+    ]);
+
+    if (error) {
+      console.log(error);
+    }
+    if (data) {
+      console.log(data);
+    }
+  };
 
   return (
     <div className="rounded-xl p-4 m-4 cursor-pointer">
@@ -69,17 +63,23 @@ const CreateNewCourse = () => {
         }`}
         onClose={() => setOpenCourse(false)}
       >
-        <div className="bg-card-bg w-fit h-fit rounded-lg p-5">
-          <div className="relative">
+        <form
+          className="bg-card-bg w-fit h-fit rounded-lg p-5"
+          onSubmit={handleSubmit}
+        >
+          <div className="relative mb-4">
             <button
-              className="text-navbar-text w-7 h-7 bg-navbar-bg rounded-full absolute right-0 top-0 flex items-center justify-center"
+              className="text-navbar-text w-7 h-7 bg-navbar-bg rounded-full absolute right-0 top-0 flex items-center justify-center text-white"
               onClick={() => setOpenCourse(false)}
             >
               <ImCross />
             </button>
+            <h1 className="text-2xl font-bold text-card-text">
+              Create New Course
+            </h1>
           </div>
           {/* semester */}
-          <div className="flex flex-col gap-2">
+          {/* <div className="flex flex-col gap-2">
             <label
               htmlFor="courseCode"
               className="text-lg font-bold text-card-text"
@@ -87,13 +87,23 @@ const CreateNewCourse = () => {
               Semester
             </label>
             <p className="border bg-white border-gray-300 rounded-md p-2">7</p>
-          </div>
+          </div> */}
           {/* coursecode */}
-          <SelectDropdown
-            label="Course Code"
-            options={courseCodes}
-            isSearchable={true}
-          />
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="courseName"
+              className="text-lg font-bold text-card-text"
+            >
+              Course Code
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Course Name"
+              className="border border-gray-300 rounded-md p-2"
+              value={courseCode}
+              onChange={(e) => setCourseCode(e.target.value)}
+            />
+          </div>
           {/* course name */}
           <div className="flex flex-col gap-2">
             <label
@@ -111,18 +121,45 @@ const CreateNewCourse = () => {
             />
           </div>
           {/* faculty */}
-          <SelectDropdown
-            label="Faculty"
-            options={facultyData}
-            isSearchable={false}
-          />
-          {/* lab */}
-          <SelectDropdown label="Lab" options={lab} isSearchable={false} />
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="courseCode"
+              className="text-lg font-bold text-card-text"
+            >
+              Faculty
+            </label>
+            <Select
+              defaultValue={faculty}
+              options={facultyData}
+              onChange={setFaculty}
+              classNamePrefix="select"
+              className="h-10 min-w-96"
+            />
+          </div>
 
-          <button className="text-navbar-text font-bold w-full bg-navbar-bg h-12 rounded-lg mt-3">
+          {/* lab */}
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="courseCode"
+              className="text-lg font-bold text-card-text"
+            >
+              Lab
+            </label>
+            <Select
+              defaultValue={lab}
+              options={labOptions}
+              onChange={setLab}
+              classNamePrefix="select"
+              className="h-10 min-w-96"
+            />
+          </div>
+          <button
+            className="text-navbar-text font-bold w-full bg-navbar-bg h-12 rounded-lg mt-3 text-white"
+            type="submit"
+          >
             Create Course
           </button>
-        </div>
+        </form>
       </div>
     </div>
   );
--- client/src/components/fa/SelectDropdown.jsx
+++ client/src/components/fa/SelectDropdown.jsx
@@ -1,24 +1,22 @@
-import React, { useState } from 'react';
-import Select from 'react-select';
-const SelectDropdown = ({ label, options, isSearchable }) => {
-  const [selectedOption, setSelectedOption] = useState();
-  console.log(isSearchable);
+// import React, { useState } from 'react';
+// import Select from 'react-select';
+// const SelectDropdown = ({ label, options }) => {
+//   const [selectedOption, setSelectedOption] = useState();
 
-  return (
-    <div className="flex flex-col gap-2">
-      <label htmlFor="courseCode" className="text-lg font-bold text-card-text">
-        {label}
-      </label>
-      <Select
-        defaultValue={selectedOption}
-        options={options}
-        isSearchable={isSearchable}
-        onChange={setSelectedOption}
-        classNamePrefix="select"
-        className="h-10 min-w-96"
-      />
-    </div>
-  );
-};
+//   return (
+//     <div className="flex flex-col gap-2">
+//       <label htmlFor="courseCode" className="text-lg font-bold text-card-text">
+//         {label}
+//       </label>
+//       <Select
+//         defaultValue={selectedOption}
+//         options={options}
+//         onChange={setSelectedOption}
+//         classNamePrefix="select"
+//         className="h-10 min-w-96"
+//       />
+//     </div>
+//   );
+// };
 
-export default SelectDropdown;
+// export default SelectDropdown;
