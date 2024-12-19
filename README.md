80404d6d9de584f50dab18c00eafca2c39b4526b
Pradnya Kumbhar <kumbharpradnyaa10@gmail.com>
2024-12-19 12:14:15 +0530

    readme

--- README.md
+++ README.md
@@ -1,3 +1,79 @@
 # Project Title
 
 Scalable Automated Academic Management Platform for Course Registration, Evaluation, and Data Handling
+
+## Features
+
+#### Student Portal
+
+- _Dashboard_: Displays personalized information such as attendance, grades, and important announcements.
+- _Semester Registration_: Enables students to register for semesters with a user-friendly interface.
+- _Attendance Tracking_: Students can monitor their attendance records for each course in real time.
+
+#### Faculty Portal
+
+- _Attendance Management_: Faculty can mark attendance for their classes and view attendance summaries.
+
+- _Personalized Evaluations_: Faculty can:
+
+  - Create personalized evaluations for their courses.
+
+  - Set maximum marks for each evaluation.
+
+  - Add marks for individual students for each evaluation.
+
+  - Automatically calculate total marks based on the evaluations.
+
+- _Grading and CGPA Calculation_:
+
+  - View a graphical representation of all student marks.
+
+  - Set cutoff marks for grades (AA, AB, BB, BC, CC, CD, DD, FF).
+
+  - Automatically calculate grades and corresponding CGPAs based on the cutoff and evaluation marks.
+
+#### Faculty Advisor Portal
+
+- _Student Monitoring_: Faculty advisors can view and manage their assigned students' academic progress and attendance.
+
+- _Course and Semester Management_:
+
+  - Create courses and assign faculties to them.
+
+  - Arrange all subjects for a particular semester into respective slots.
+
+  - Create and float semester registration forms.
+
+  - Receive and manage all course-wise registration data for a particular batch.
+
+- _Backlog Management_:
+
+  - Add backlog students to the system.
+
+  - Prevent backlog students from registering for new courses until backlogs are cleared.
+
+- _Communication_: Send notices to an entire batch or a specific student.
+
+## Deployed Application
+
+Access the deployed application here: https://classync.onrender.com/
+
+### Login Credentials
+
+#### Student Portal
+
+_Usernames_: bt21ece001@iiitn.ac.in - bt21ece002@iiitn.ac.in ......... bt21ece010@iiitn.ac.in ....... bt21ece50@iiitn.ac.in
+
+_Password_: 1
+
+#### Faculty Portal
+
+_Usernames_: tapan@gmail.com, paritosh@gmail.com, kamaljeet@gmail.com, sushmita@gmail.com, vikrant@gmail.com
+
+_Password_: 1
+
+#### Faculty Advisor Portal
+
+_Username_: fa
+
+_Password_: 1
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,137 +1,21 @@
-// import { useContext, useState, useEffect } from 'react';
-// import { AuthContext } from '../context/Authcontext';
-// import supabase from '../config/supabaseClient';
-
-// const Login = () => {
-//   const { setEmail, setIsAuthenticated } = useContext(AuthContext);
-//   const [username, setUsername] = useState('');
-//   const [password, setPassword] = useState('');
-//   const [errorMessage, setErrorMessage] = useState('');
-//   const [facultyEmails, setFacultyEmails] = useState([]);
-
-//   useEffect(() => {
-//     // Fetch faculty emails from the "faculties" table
-//     const fetchFacultyEmails = async () => {
-//       const { data, error } = await supabase
-//         .from('faculties')
-//         .select('gmail');
-
-//       if (error) {
-//         console.error("Error fetching faculty emails:", error);
-//       } else {
-//         // Extract email addresses from the fetched data
-//         const emails = data.map(faculty => faculty.gmail);
-//         setFacultyEmails(emails);
-//       }
-//     };
-
-//     fetchFacultyEmails();
-//   }, []);
-
-//   const handleLogin = (e) => {
-//     e.preventDefault();
-
-//     const studentEmailPattern =
-//       /^bt21ece(0[0-9]{2}|1[01][0-9]|130)@iiitn\.ac\.in$/;
-//     const isStudentEmail = studentEmailPattern.test(username);
-
-//     if (isStudentEmail && password === '1') {
-//       setEmail(username);
-//       setIsAuthenticated(true);
-//       window.location.href = '/student/dashboard'; // Redirect after successful login
-//     } else if (username === 'fa' && password === 'fa') {
-//       setEmail(username);
-//       setIsAuthenticated(true);
-//       window.location.href = '/fa/dashboard';
-//     } else if (facultyEmails.includes(username) && password === '1') {
-//       setEmail(username);
-//       setIsAuthenticated(true);
-//       window.location.href = '/faculty/dashboard';
-//     } else if (username === 'admin' && password === 'admin') {
-//       setEmail(username);
-//       setIsAuthenticated(true);
-//       window.location.href = '/admin/dashboard';
-//     } else {
-//       setErrorMessage('Invalid username or password');
-//     }
-//   };
-
-//   return (
-//     <div className="flex min-h-screen items-center justify-center bg-gray-900 text-gray-300">
-//       <div className="w-full max-w-md px-6 py-10 bg-gray-800 rounded-3xl shadow-2xl">
-//         <div className="text-center mb-8">
-//           <h4 className="text-2xl font-bold text-gray-100 mb-2">
-//             Welcome Back!
-//           </h4>
-//           <p className="text-gray-400 text-sm">
-//             Sign in to access your dashboard
-//           </p>
-//         </div>
-//         <form onSubmit={handleLogin}>
-//           <div className="mb-6">
-//             <label
-//               htmlFor="username"
-//               className="block text-sm font-medium text-gray-300 mb-2"
-//             >
-//               Username
-//             </label>
-//             <input
-//               type="text"
-//               id="username"
-//               placeholder="Enter your email"
-//               className="w-full px-4 py-2 border border-gray-600 rounded-lg bg-gray-700 text-gray-200 focus:ring-2 focus:ring-blue-500 focus:outline-none"
-//               onChange={(e) => setUsername(e.target.value)}
-//             />
-//           </div>
-//           <div className="mb-6">
-//             <label
-//               htmlFor="password"
-//               className="block text-sm font-medium text-gray-300 mb-2"
-//             >
-//               Password
-//             </label>
-//             <input
-//               type="password"
-//               id="password"
-//               placeholder="••••••••"
-//               className="w-full px-4 py-2 border border-gray-600 rounded-lg bg-gray-700 text-gray-200 focus:ring-2 focus:ring-blue-500 focus:outline-none"
-//               onChange={(e) => setPassword(e.target.value)}
-//             />
-//           </div>
-//           {errorMessage && (
-//             <p className="text-sm text-red-500 mb-4">{errorMessage}</p>
-//           )}
-//           <button
-//             type="submit"
-//             className="w-full py-2 px-4 bg-gradient-to-r from-purple-600 to-blue-500 text-white font-bold rounded-lg shadow-md hover:shadow-lg hover:opacity-90 transform hover:-translate-y-1 transition-all duration-300"
-//           >
-//             Sign In
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Login;
-import React, { useContext, useState, useEffect } from "react";
-import { AuthContext } from "../context/Authcontext";
-import supabase from "../config/supabaseClient";
-import logo from "../assets/iiitn.png";
+import React, { useContext, useState, useEffect } from 'react';
+import { AuthContext } from '../context/Authcontext';
+import supabase from '../config/supabaseClient';
+import logo from '../assets/iiitn.png';
 
 const Login = () => {
   const { setEmail, setIsAuthenticated } = useContext(AuthContext);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
   const [facultyEmails, setFacultyEmails] = useState([]);
 
   useEffect(() => {
     const fetchFacultyEmails = async () => {
-      const { data, error } = await supabase.from("faculties").select("gmail");
+      const { data, error } = await supabase.from('faculties').select('gmail');
 
       if (error) {
-        console.error("Error fetching faculty emails:", error);
+        console.error('Error fetching faculty emails:', error);
       } else {
         const emails = data.map((faculty) => faculty.gmail);
         setFacultyEmails(emails);
@@ -148,24 +32,20 @@ const Login = () => {
       /^bt21ece(0[0-9]{2}|1[01][0-9]|130)@iiitn\.ac\.in$/;
     const isStudentEmail = studentEmailPattern.test(username);
 
-    if (isStudentEmail && password === "1") {
+    if (isStudentEmail && password === '1') {
       setEmail(username);
       setIsAuthenticated(true);
-      window.location.href = "/student/dashboard";
-    } else if (username === "fa" && password === "fa") {
+      window.location.href = '/student/dashboard';
+    } else if (username === 'fa' && password === '1') {
       setEmail(username);
       setIsAuthenticated(true);
-      window.location.href = "/fa/dashboard";
-    } else if (facultyEmails.includes(username) && password === "1") {
+      window.location.href = '/fa/dashboard';
+    } else if (facultyEmails.includes(username) && password === '1') {
       setEmail(username);
       setIsAuthenticated(true);
-      window.location.href = "/faculty/dashboard";
-    } else if (username === "admin" && password === "admin") {
-      setEmail(username);
-      setIsAuthenticated(true);
-      window.location.href = "/admin/dashboard";
+      window.location.href = '/faculty/dashboard';
     } else {
-      setErrorMessage("Invalid username or password");
+      setErrorMessage('Invalid username or password');
     }
   };
 
@@ -224,6 +104,9 @@ const Login = () => {
             Sign In
           </button>
         </form>
+        <span className="flex justify-center items-center m-auto">
+          Refer README for login credentials
+        </span>
       </div>
     </div>
   );
